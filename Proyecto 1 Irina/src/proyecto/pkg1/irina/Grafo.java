40cdeb30e3550db659c14061254ada632b147b19
irispacoder <irinaspa@correo.unimet.edu.ve>
2025-11-03 01:17:07 -0400

    Se agrego la clase Grafo con getters y el metodo buscar usuario

--- Proyecto 1 Irina/src/proyecto/pkg1/irina/Grafo.java
+++ Proyecto 1 Irina/src/proyecto/pkg1/irina/Grafo.java
@@ -5,5 +5,47 @@
 package proyecto.pkg1.irina;
 
 public class Grafo {
+    private Lista<InfoUsuario> users;
+    private int cantidad;
+    
+    /** se utiliza para pasar la cantidad de usuarios */
+    public Grafo(){
+        this.users = new Lista<>();
+        this.cantidad = 0;
+    }
+    
+    /** busca el usuario del que se quiere saber */
+    public InfoUsuario searchUser(String nombre){
+        Nodo<InfoUsuario> aux = this.users.pfirst;
+        while (aux != null){
+            if (aux.getDato().nombre.equals(nombre)){
+            return aux.getDato();
+            }
+        aux = aux.getPnext();
+        }
+    return null;
+    }
+    
+    
+    
+    /** getters */
+
+    /**
+     * @return the users
+     */
+    public Lista<InfoUsuario> getUsers() {
+        return users;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+    
+    
+    
     
 }
+
